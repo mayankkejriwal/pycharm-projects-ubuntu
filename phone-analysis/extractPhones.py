892622b6bcc49ec923b67affa0808ab87fec56fd
Mayank Kejriwal <mayankkejriwal@Mayanks-iMac.local>
2017-05-25 10:44:17 -0700

    phone analysis exerimental code

--- phone-analysis/extractPhones.py
+++ phone-analysis/extractPhones.py
@@ -4,6 +4,18 @@ import json, re
 import gzip
 import glob
 
+
+def print_first_n_lines(gz_file='/Users/mayankkejriwal/datasets/memex/user-worker-dig3-full-phase4-2017-feb/gz/part-00000.txt.gz',
+                        n=1):
+    count = 0
+    with gzip.open(gz_file, 'rb') as f:
+        for line in f:
+            print line
+            count += 1
+            if count == n:
+                break
+
+
 def extract_phones_from_all_files(outer_folder=
                 '/Users/mayankkejriwal/datasets/memex/user-worker-dig3-full-phase4-2017-feb/'):
     """
@@ -40,6 +52,42 @@ def extract_phones_from_all_files(outer_folder=
     out_relaxed.close()
 
 
+def extract_phones_urls_from_all_files(outer_folder=
+                '/Users/mayankkejriwal/datasets/memex/user-worker-dig3-full-phase4-2017/'):
+    """
+    Will write out strict phones in outer_folder+'strict_phones.txt' and relaxed_phones in outer_folder+'relaxed_phones.txt'
+    Note that while we do 'set' checking for each individual json there can be many repeated phones across the file.
+    Each line will be tab-delimited and represent the phones per json.
+    :param outer_folder:
+    :return:
+    """
+    jpath_relaxed = parse('fields.phone.relaxed[*].name')
+    jpath_strict = parse('fields.phone.strict[*].name')
+    listOfFiles = glob.glob(outer_folder + 'gz/*.txt.gz')
+    out_strict = codecs.open(outer_folder+'strict_phones_urls.txt', 'w', 'utf-8')
+    out_relaxed = codecs.open(outer_folder + 'relaxed_phones_urls.txt', 'w', 'utf-8')
+    for fi in listOfFiles:
+        print 'processing file: ',fi
+        with gzip.open(fi, 'rb') as f:
+            for line in f:
+                j = json.loads(re.split('\t', line[0:-1])[1])
+
+                strict_phones = list(set([match.value for match in jpath_strict.find(j)]))
+                if len(strict_phones) != 0:
+                    strict_phones = '\t'.join(strict_phones)
+                    out_strict.write(j['url']+'\t'+strict_phones)
+                    out_strict.write('\n')
+
+                relaxed_phones = list(set([match.value for match in jpath_relaxed.find(j)]))
+                if len(relaxed_phones) != 0:
+                    relaxed_phones = '\t'.join(relaxed_phones)
+                    out_relaxed.write(j['url']+'\t'+relaxed_phones)
+                    out_relaxed.write('\n')
+
+    out_strict.close()
+    out_relaxed.close()
+
+
 def extract_2017_phones_from_nebraska_file(jl_file1, jl_file2, output_file):
     out = codecs.open(output_file, 'w', 'utf-8')
     with codecs.open(jl_file1, 'r', 'utf-8') as f:
@@ -62,5 +110,39 @@ def extract_2017_phones_from_nebraska_file(jl_file1, jl_file2, output_file):
     print 'finished processing second jl file'
     out.close()
 
-path = '/Users/mayankkejriwal/datasets/memex/nebraska-data/memex_comm/'
-extract_2017_phones_from_nebraska_file(path+'data_for_memex_1.json', path+'data_for_memex_2.json', path+'phones-2017.txt')
+
+def extract_2017_phones_urls_from_nebraska_file(jl_file1, jl_file2, output_file):
+    out = codecs.open(output_file, 'w', 'utf-8')
+    with codecs.open(jl_file1, 'r', 'utf-8') as f:
+        for line in f:
+            obj = json.loads(line)
+            if 'day' in obj and str(obj['day'])[0:4] == '2017':
+                if 'phone' in obj and len(obj['phone']) > 0:
+                    k = '\t'.join(obj['phone'])
+                    out.write(obj['url']+'\t'+k)
+                    out.write('\n')
+    print 'finished processing first jl file'
+    with codecs.open(jl_file2, 'r', 'utf-8') as f:
+        for line in f:
+            obj = json.loads(line)
+            if 'day' in obj and str(obj['day'])[0:4] == '2017':
+                if 'phone' in obj and len(obj['phone']) > 0:
+                    k = '\t'.join(obj['phone'])
+                    out.write(obj['url']+'\t'+k)
+                    out.write('\n')
+    print 'finished processing second jl file'
+    out.close()
+
+
+# def deduplicate_file_by_url(phone_url_file):
+#     with codecs.open(phone_url_file, 'r', 'utf-8') as f:
+#         for line in f:
+#             elements = re.split('\t', line[0:-1])
+
+
+
+# print_first_n_lines()
+# extract_phones_urls_from_all_files()
+# path = '/Users/mayankkejriwal/datasets/memex/nebraska-data/memex_comm/'
+# extract_2017_phones_from_nebraska_file(path+'data_for_memex_1.json', path+'data_for_memex_2.json', path+'phones-2017.txt')
+# extract_2017_phones_urls_from_nebraska_file(path+'data_for_memex_1.json', path+'data_for_memex_2.json', path+'phones-urls-2017.txt')
--- phone-analysis/intersectPhones.py
+++ phone-analysis/intersectPhones.py
@@ -1,4 +1,5 @@
 import codecs, re, json
+import math
 
 
 def compute_intersection_metrics(nebraska_phones, memex_phones):
@@ -17,12 +18,113 @@ def compute_intersection_metrics(nebraska_phones, memex_phones):
                 phone_set_2.add(p)
     print 'len of unique memex phones is ', str(len(phone_set_2))
     inter = phone_set_1.intersection(phone_set_2)
+    print 'some phones in both memex and nebraska...'
+    count = 0
+    for p in inter:
+        if count > 5:
+            break
+        print p
+        count += 1
     union = phone_set_1.union(phone_set_2)
     print 'intersection length: ',str(len(inter))
     print 'union length: ', str(len(union))
     jacc = (1.0*len(inter))/len(union)
     print 'jaccard: ',str(jacc)
 
+
+def compute_url_intersection_metrics(nebraska_phones, memex_phones):
+    phone_dict_1 = dict()
+    nebraska_count = 0
+    phone_dict_2 = dict()
+    memex_count = 0
+    with codecs.open(nebraska_phones, 'r', 'utf-8') as f:
+        for line in f:
+            elements = re.split('\t', line[0:-1])
+            if elements[0] not in phone_dict_1:
+                phone_dict_1[elements[0]] = set()
+                nebraska_count += 1
+            for p in elements[1:]:
+                phone_dict_1[elements[0]].add(p)
+    print 'len of unique nebraska urls is ', str(nebraska_count)
+
+    with codecs.open(memex_phones, 'r', 'utf-8') as f:
+        for line in f:
+            elements = re.split('\t', line[0:-1])
+            if elements[0] not in phone_dict_2:
+                phone_dict_2[elements[0]] = set()
+                memex_count += 1
+            for p in elements[1:]:
+                phone_dict_2[elements[0]].add(p)
+    print 'len of unique memex urls is ', str(memex_count)
+
+    p1 = 0
+    p12 = 0
+
+    # print 'http://southernmaryland.backpage.com/WomenSeekMen/real-exotic-busty-beauty-five-companionship/26526979' in phone_dict_2
+    # print 'http://southernmaryland.backpage.com/WomenSeekMen/real-exotic-busty-beauty-five-companionship/26526979' in phone_dict_1
+
+    for k in phone_dict_1.keys():
+        if k not in phone_dict_2:
+            p1 += 1
+        else:
+            p12 += 1
+            del phone_dict_2[k]
+    p2 = memex_count-p12
+
+    print 'num. of urls common to memex and nebraska...',str(p12)
+    print 'num. of urls in nebraska but not memex',str(p1)
+    print 'num. of urls in memex but not nebraska',str(p2)
+
+
+def compute_phone_intersection_metrics_by_common_urls(nebraska_phones, memex_phones):
+    phone_dict_1 = dict()
+    nebraska_count = 0
+    phone_dict_2 = dict()
+    memex_count = 0
+    with codecs.open(nebraska_phones, 'r', 'utf-8') as f:
+        for line in f:
+            elements = re.split('\t', line[0:-1])
+            if elements[0] not in phone_dict_1:
+                phone_dict_1[elements[0]] = set()
+                nebraska_count += 1
+            for p in elements[1:]:
+                phone_dict_1[elements[0]].add(p)
+    # print 'len of unique nebraska urls is ', str(nebraska_count)
+
+    with codecs.open(memex_phones, 'r', 'utf-8') as f:
+        for line in f:
+            elements = re.split('\t', line[0:-1])
+            if elements[0] not in phone_dict_1:
+                continue # no need to store
+            if elements[0] not in phone_dict_2:
+                phone_dict_2[elements[0]] = set()
+                memex_count += 1
+            for p in elements[1:]:
+                phone_dict_2[elements[0]].add(p)
+    # print 'len of unique memex urls is ', str(memex_count)
+
+    j = 0
+    j_std = 0
+    count = 0
+    for k in phone_dict_2.keys():
+        if k not in phone_dict_1:
+            raise Exception
+        else:
+            jacc = 1.0*len(phone_dict_1[k].intersection(phone_dict_2[k]))/len(phone_dict_1[k].union(phone_dict_2[k]))
+            j += jacc
+            j_std += (jacc*jacc)
+            count += 1
+    avg_jacc = j/count
+    j_std = j_std/count
+    j_std = math.sqrt(j_std-(avg_jacc*avg_jacc))
+    print 'average jaccard is ',str(avg_jacc)
+    print 'std. dev. is ',str(j_std)
+
+
+
+
 nebraska_path = '/Users/mayankkejriwal/datasets/memex/nebraska-data/memex_comm/'
 memex_path = '/Users/mayankkejriwal/datasets/memex/'
-compute_intersection_metrics(nebraska_path+'phones-2017.txt', memex_path+'2017-relaxed-phones.txt')
+compute_phone_intersection_metrics_by_common_urls(nebraska_path+'phones-urls-2017.txt', memex_path+'2017-relaxed-phones-urls.txt')
+# compute_url_intersection_metrics(nebraska_path+'phones-urls-2017.txt', memex_path+'2017-relaxed-phones-urls.txt')
+# compute_intersection_metrics(nebraska_path+'phones-2017.txt', memex_path+'2017-strict-phones.txt')
